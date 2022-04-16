c6ddadb866ca664cd6fe3b6381208132f9b5da71
Citeli-py <81636534+Citeli-py@users.noreply.github.com>
2022-04-16 18:43:23 -0300

    Update main.c

--- ABB/main.c
+++ ABB/main.c
@@ -7,12 +7,11 @@ void menu()
     printf( "\n\n1- Ler uma arvore de um arquivo\n"
             "2- Imprimir a arvore (pre-ordem, em-ordem, pos-ordem e em largura)\n"
             "3- Verificar se um elemento x existe\n"
-            "4- Contar o numero de elementos na arvore\n"
-            "5- Imprimir os nos folhas\n"
-            "6- Verificar se uma arvore esta balanceada\n"
-            "7- Verificar se uma arvore eh cheia\n"
-            "8- Imprimir o nivel de um no\n"
-            "9- Sair\n"
+            "4- Imprimir o nivel de um no x\n"
+            "5- Imprimir as folhas menores que um valor x\n"
+            "6- Inserir um no x na arvore\n"
+            "7- Remover um no x da arvore\n"
+            "8- Sair\n"
             "|Escolha> ");
 }
 
@@ -23,7 +22,7 @@ int main()
     int op=0, aux;
     char arquivo[30];
 
-    while (op != 9)
+    while (op != 8)
     {
         menu();
         scanf("%d", &op);
@@ -50,44 +49,41 @@ int main()
                 break;
             
             case 4:
-                aux = ContarNos(a);
-                printf("A arvore tem %d nos", aux);
-                break;
-
-            case 5:
-                printf("Folhas: ");
-                Folhas(a);
-                break;
-
-            case 6:
-                if(Balanceada(a))
-                    printf("A arvore eh balanceada!");
-                else
-                    printf("A arvore nao eh balanceada!");
-                break;
-            
-            case 7:
-                if(Cheia(a))
-                    printf("A arvore eh cheia!");
-                else
-                    printf("A arvore nao eh cheia!");
-                break;
-
-            case 8:
-                printf("Digite o no: ");
+                printf("Qual elemento: ");
                 scanf("%d", &aux);
                 NivelNo(a, aux, 0);
                 break;
+
+            case 5:
+                printf("Qual elemento: ");
+                scanf("%d", &aux);
+                printf("Folhas menores que %d: ", aux);
+                Folhas(a, aux);
+                break;
+
+            case 6:
+                printf("Insira um valor: ");
+                scanf("%d", &aux);
+                a = inserir(a, aux);
+                break;
             
-            case 9:
+            case 7:
+                printf("Remover qual elemento: ");
+                scanf("%d", &aux);
+                a = remover(a, aux);
+                break;
+
+            case 8:
                 break;
 
             default:
                 op = 0;
+                printf("Entrada invalida!");
                 break;
         }
     }
     fclose(f);
     libera_arvore(a);
+    printf("\nPrograma Finalizado!\n");
     return 0;
 }
